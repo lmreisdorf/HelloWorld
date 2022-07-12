3e15a1735abb6c8fb17951e1ddef0dda200a1454
Lynette Reisdorf <LMREISDORF@GMAIL.COM>
2022-07-12 16:22:25 -0700

    Updated version

--- app/src/main/java/com/example/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/myhelloworld/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.myhelloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupHyperlink()
         val button = findViewById<Button>(R.id.helloButton)
         button.setOnClickListener {
             Log.v("Helloworld", "Button clicked!")
             Toast.makeText(this, "Hello to you too!", Toast.LENGTH_SHORT).show()
         }
+
+        }
+    fun setupHyperlink() {
+        val linkTextView = findViewById<TextView>(R.id.introtextView)
+        linkTextView.movementMethod = LinkMovementMethod.getInstance()
     }
 }
+
+
